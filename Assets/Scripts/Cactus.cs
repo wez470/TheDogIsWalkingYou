6e901d6160257d2faaf21183bf3e98a4c71bfaab
Aoliv <abigailmoliver@gmail.com>
2017-11-19 14:28:05 -0600

    cacti don't move on game over

--- Assets/Scripts/Cactus.cs
+++ Assets/Scripts/Cactus.cs
@@ -6,13 +6,12 @@ public class Cactus : MonoBehaviour {
     private GameObject gameOverMenu;
 
     // Use this for initialization
-    void Start () {
-        gameOverMenu = GameObject.FindGameObjectWithTag("GameOverMenu");	
+    void Start () {	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (!gameOverMenu.active)
+        if (!GameObject.FindGameObjectWithTag("GameOverMenu"))
         {
             Vector3 newPosition = transform.position;
             newPosition.x -= 0.05f;
