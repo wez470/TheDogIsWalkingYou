f249f40cf9a5fed1306d6f393925e9fe21166229
Weston <wez470@gmail.com>
2017-11-19 12:39:04 -0600

    Add cactus spawner.

---
+++ Assets/Scripts/Cactus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cactus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPosition = transform.position;
+        newPosition.x -= 0.05f;
+        transform.position = newPosition;
+	}
+}
---
+++ Assets/Scripts/CactusSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusSpawner : MonoBehaviour {
+    public List<GameObject> cacti = new List<GameObject>();
+    public float LowestTimeBetweenSpawns = 1f;
+    public float HightestTimeBetweenSpawns = 3f;
+
+    private float nextSpawnTime = 0f;
+
+	void Start () {
+        this.nextSpawnTime = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.timeSinceLevelLoad > nextSpawnTime) {
+            GameObject cactusPrefab = cacti[Random.Range(0, cacti.Count)];
+            GameObject newCactus = (GameObject)Instantiate(cactusPrefab);
+            newCactus.transform.position = this.transform.position;
+            this.nextSpawnTime = Time.timeSinceLevelLoad + Random.Range(LowestTimeBetweenSpawns, HightestTimeBetweenSpawns);
+        }
+
+    }
+}
