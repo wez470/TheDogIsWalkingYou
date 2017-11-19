51064f8578422123bbc272407232891a013c4271
Weston <wez470@gmail.com>
2017-11-19 10:56:57 -0600

    Add initial leash object.

--- Assets/Scripts/Players/Leash.cs
+++ Assets/Scripts/Players/Leash.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Leash : MonoBehaviour {
+    public Transform DogHand;
+    public Transform HumanCollar;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start () {}
 	
-	// Update is called once per frame
 	void Update () {
-		
+        setPosition();
+        setScale();
+        setRotation();
 	}
+
+    private void setPosition() {
+        transform.position = (DogHand.position + HumanCollar.position) / 2;
+    }
+
+    private void setScale() {
+        float yScale = Vector3.Distance(DogHand.transform.position, HumanCollar.position);
+        Vector3 scale = transform.localScale;
+        scale.y = yScale;
+        transform.localScale = scale;
+    }
+
+    private void setRotation() {
+        Vector3 rotation = DogHand.position - HumanCollar.position;
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        Quaternion.Euler(new Vector3(0, 0, rotZ));
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, rotZ + 90));
+    }
 }
