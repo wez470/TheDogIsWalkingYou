0a480766efb849880a2415d178000371cdff3625
Aoliv <abigailmoliver@gmail.com>
2017-11-19 10:05:28 -0700

    repeating background

--- Assets/Scripts/repeatingBackground.cs
+++ Assets/Scripts/repeatingBackground.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class repeatingBackground : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private BoxCollider2D groundCollider;    
+    private float groundHorizontalLength;
+
+    private void Awake()
+    {
+        groundCollider = GetComponent<BoxCollider2D>();
+        groundHorizontalLength = groundCollider.size.x;
+    }
+
+    void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
-	}
+        Vector3 position = transform.position;
+        position.x -= 0.05f;
+        transform.position = position;
+
+        if (transform.position.x < -groundHorizontalLength)
+        {
+            RepositionBackground();
+        }
+    }
+
+    private void RepositionBackground()
+    {
+        Vector2 groundOffSet = new Vector2(groundHorizontalLength * 2f, 0);
+        transform.position = (Vector2)transform.position + groundOffSet;
+    }
 }
