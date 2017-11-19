ff95d319b501228d891c6f1600f1dfc811b65b02
Aoliv <abigailmoliver@gmail.com>
2017-11-19 15:02:27 -0600

    Apply

--- Assets/Scripts/Players/Dog.cs
+++ Assets/Scripts/Players/Dog.cs
@@ -5,16 +5,6 @@ using UnityEngine;
 public class Dog : MonoBehaviour {
     public GameController gameController;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     void OnCollisionEnter2D(Collision2D coll) {
         gameController.GameOver();
     }
---
+++ Assets/Scripts/Players/Jump.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rigidbody2D;
+    private bool grounded = false;
+    public float maxSpeed = 10f;
+    public float jumpForce = 800f;
+
+    // Use this for initialization
+    void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2D.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    // FixedUpdate is called at a set rate
+    void FixedUpdate()
+    {
+
+       // grounded = boxCollider.IsTouching();
+        if (grounded)
+        {
+            grounded = true;
+        }
+
+        float move = Input.GetAxis("Horizontal");
+        rigidbody2D.velocity = new Vector2(maxSpeed * move, rigidbody2D.velocity.y);
+    }
+}
